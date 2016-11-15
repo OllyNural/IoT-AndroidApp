f38fcda4aad37604796bc01d5173f1ab724a13c6
Olly Nural <Olly Nural>
2016-11-15 18:14:43 +0000

    Commented everything out and made cleaner

--- app/src/main/java/ollynural/iot_androidapp/MainActivity.java
+++ app/src/main/java/ollynural/iot_androidapp/MainActivity.java
@@ -1,5 +1,9 @@
 package ollynural.iot_androidapp;
 
+/**
+ *  @Author Oliver Nural - on36@kent.ac.uk
+ */
+
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
@@ -16,10 +20,11 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothLeScanner bluetoothLeScanner;
 
-    // Array list so you can add as many iBeacons as you want. This future proofs the app.
+    // Array list so you can add as many iBeacons as you want, future proofing the app.
     private static final List<String> MAC_ADDRESS = new ArrayList<>(Collections.singletonList("C5:04:72:47:93:60"));
-    // To prevent false positives I use a boolean
+    // To prevent false positives I use a boolean set each time the status changes
     private boolean repeatCheckBoolean = false;
+    // To check if a message has already been sent whilst in range/out of range
     private boolean hasSentInRangeMessage = false;
     private boolean hasSentOutRangeMessage = false;
 
@@ -27,12 +32,21 @@ public class MainActivity extends AppCompatActivity {
     MqttClient mqttClient = new MqttClient();
 
     // Gets the Unique Android ID for the device the user is running the app
-    // Due to context not setting early enough this can't be final and I have to change this during onCreate()
+    // Due to application context not setting early enough this can't be final and I have to change this during onCreate()
     private String android_id = "";
 
+    /**
+     * On create of app this function will initiate our Mqtt Connection,
+     * retrieve android_id from device and start scanning for Bluetooth LE devices
+     *
+     * This also pings the mqtt server every few seconds to ensure the connection
+     * is kept alive as I have told it to
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        System.out.println("Creating APP");
+        // Setup of App
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Set up the connection to the server
@@ -42,15 +56,22 @@ public class MainActivity extends AppCompatActivity {
         // Set up Bluetooth Adapter and Scanner
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         bluetoothLeScanner = bluetoothAdapter.getBluetoothLeScanner();
-        // Start Scanning!
-        scanLeDevice();
-    }
-
-    private void scanLeDevice() {
+        // Start Scanning!WW
         bluetoothLeScanner.startScan(scanCallback);
         System.out.println("Started scanning! for id: " + android_id);
     }
 
+
+    /**
+     * Callback for the Bluetooth LE Scanner
+     * Checks if beacon is in list of our iBeacons, in or out of range, in the correct state and
+     * if we have already sent a message for this state. If all criteria met, this will call
+     * MqttClient.sendMessage to send a message to the MQTT server.
+     *
+     * repeatCheckBoolean states are as follows:
+     * when equal to false this means the last reading was 'out of range'
+     * when equal to true, this means the last reading was 'in range'
+     */
     private final ScanCallback scanCallback = new ScanCallback() {
         @Override
         public void onScanResult(int callbackType, ScanResult result) {
@@ -58,35 +79,37 @@ public class MainActivity extends AppCompatActivity {
             if (MAC_ADDRESS.contains(result.getDevice().getAddress())) {
                 System.out.println("Found iBeacon!");
                 if (result.getRssi() > -60) {
-                    // Has come in range twice now, so should be correct
+                    // Check if false positive or if it has been  'in range' twice now
                     if (repeatCheckBoolean) {
+                        // Has come in range twice now, so should be correct
                         System.out.println("IN RANGE RSSI: " + result.getRssi());
-                        // Check if a message has already been sent for in range
+                        // Check if a message has already been sent for in range since being in range
                         if (!hasSentInRangeMessage) {
                             mqttClient.sendMessage(android_id, result.getDevice().getAddress(), true);
-                            // Set sent in range to true and reset out of range
+                            // Set sent in range to true and reset out of range to false
                             hasSentInRangeMessage = true;
+                            hasSentOutRangeMessage = false;
                         }
                     }
-                    // Check if just a false positive, as was just out of range
+                    // Check if it is a false 'in range' positive, as was just out of range
                     else {
-                        System.out.println("Was just OUT of range, trying again!");
                         repeatCheckBoolean = true;
                     }
                 } else {
-                    // Has left range twice now, so should be correct
+                    // Check if false positive or if it has been  'out of range' twice now
                     if (!repeatCheckBoolean) {
+                        // Has been out of range twice now, so should be correct
                         System.out.println("OUT OF RANGE RSSI: " + result.getRssi());
-                        // Check if a message has already been sent for out of range
+                        // Check if a message has already been sent for out of range since being out of range
                         if (!hasSentOutRangeMessage) {
                             mqttClient.sendMessage(android_id, result.getDevice().getAddress(), false);
-                            // Set out of range to true and reset in range
+                            // Set out of range to true and reset in range to false;
                             hasSentOutRangeMessage = true;
+                            hasSentInRangeMessage = false;
                         }
                     }
-                    // Check if just a false positive, as was just in range
+                    // Check if it is a false 'out of range' positive, as was just in range
                     else {
-                        System.out.println("Was just IN of range, trying again!");
                         repeatCheckBoolean = false;
                     }
                 }
--- app/src/main/java/ollynural/iot_androidapp/MqttClient.java
+++ app/src/main/java/ollynural/iot_androidapp/MqttClient.java
@@ -1,7 +1,7 @@
 package ollynural.iot_androidapp;
 
 /**
- * Created by Oliver Nural on 11/14/2016.
+ *  @Author Oliver Nural - on36@kent.ac.uk
  */
 
 import org.eclipse.paho.client.mqttv3.*;
@@ -9,61 +9,70 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 class MqttClient {
 
-    // Uses StringBuffer over StringBuilder and String as threadsafe and more efficient than String
-    private final StringBuffer topic        = new StringBuffer("unikent/users/on36/beaconTopic");
-    // Quality of Service should be 2 - Exactly Once
-    // Slower but more secure and will always get to the server which is what we want
-    private final int qos                   = 1;
-//    private final String broker             = "tcp://co657-mqtt.kent.ac.uk:1883";
-//    private final String broker             = "tcp://129.12.44.120:1883";
-    private final String broker               = "tcp://iot.eclipse.org:1883";
+    // Setup Broker URI and Topic
+    private final String BROKER = "tcp://iot.eclipse.org:1883";
+    private final String TOPIC  = "on36/iot-beacon";
 
+    // Quality of Service should be 2 - Should send exactly once
+    // Slower but more secure and will always get to the server which is what we want
+    private final int qos       = 1;
+
+    // Memory persistence for the MqttAsyncClient
     private MemoryPersistence persistence = new MemoryPersistence();
+
     // Set up as global variable to allow pinging and sending messages
     private MqttAsyncClient mqttAsyncClient = null;
 
+    /**
+     * Sets up the connection to the MQTT server and connects
+     * Connection is set to keep alive with a duration of every 60 seconds.
+     */
     void setUpConnection() {
-        System.out.println("Setting up connection!");
+        System.out.println("Setting up connection to: " + BROKER);
         try {
-            mqttAsyncClient = new MqttAsyncClient(broker, "", persistence);
+            // Initialise the global client with uri, clientId and persistence
+            mqttAsyncClient = new MqttAsyncClient(BROKER, "", persistence);
+            // Add Options of starting clean session and connection keep alive time
             MqttConnectOptions connectOptions = new MqttConnectOptions();
             connectOptions.setCleanSession(true);
-            System.out.println("Connecting to broker: " + broker);
-            IMqttToken iMqttToken = mqttAsyncClient.connect(connectOptions);
-            System.out.println(iMqttToken);
-            System.out.println("Connected!");
+            connectOptions.setKeepAliveInterval(60);
+            // Connect using connectOptions to the broker
+            mqttAsyncClient.connect(connectOptions);
+            System.out.println("Connected to: " + BROKER);
         } catch (MqttException e) {
             System.out.println("Except: " + e);
             e.printStackTrace();
         }
     }
 
-    void disconnect() {
-        try {
-            mqttAsyncClient.disconnect();
-        } catch (MqttException e) {
-            System.out.println("Except: " + e);
-            e.printStackTrace();
-        }
-        System.out.println("Disconnected!");
-    }
-
+    /**
+     * Send message to the MQTT Server
+     * Converts rangeStatus to bytes, and passes as message, setting QoS as 2 - Exactly Once
+     * Publishes message on TOPIC  with above message
+     *
+     * @param ANDROID_ID    - Id of the user's device
+     * @param mac_address   - Mac address of the iBeacon
+     * @param rangeStatus   - Whether the iBeacon is in range or out of range
+     */
     void sendMessage(String ANDROID_ID, String mac_address, boolean rangeStatus) {
         System.out.println("~~~Received request to send to MQTT Server:");
         System.out.println("~~~ANDROID_ID: " + ANDROID_ID);
         System.out.println("~~~Mac_addresss: " + mac_address);
         System.out.println("~~~Range Status: " + rangeStatus);
 
-        // Set up sending data
-//        topic.append(mac_address);
-        StringBuffer content = new StringBuffer("");
+        // Set up data to send
+        // Uses StringBuilder as more efficient than String
+        StringBuilder content = new StringBuilder("");
         content.append(rangeStatus);
-        // Send as bytes
+
+        // Send message as bytes
         MqttMessage mqttMessage = new MqttMessage(content.toString().getBytes());
+        // Add QOS
         mqttMessage.setQos(qos);
         try {
-            mqttAsyncClient.publish(topic.toString(), mqttMessage);
-            System.out.println("Message published! " + topic.toString() + " " + mqttMessage);
+            // Publish message on TOPIC
+            mqttAsyncClient.publish(TOPIC, mqttMessage);
+            System.out.println("Message published! " + "\n" + "Topic: " + TOPIC + "\n" + "Message: " + mqttMessage);
         } catch (MqttException e) {
             System.out.println("Except: " + e);
             e.printStackTrace();
